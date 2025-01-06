10b1b3b010e4973593a8bc61e7fc52d314546ceb
Anna <annagennadinik@gmail.com>
2025-01-06 15:14:45 -0800

    routes for apis

---
+++ api/index.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+### Create FastAPI instance with custom docs and openapi url
+app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust to your needs
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/helloFastApi")
+def hello_fast_api():
+    return {"message": "Hello from FastAPI"}
