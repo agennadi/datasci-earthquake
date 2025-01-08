6bed7b1582f5011f6afd2114e2cd9388d555b770
Anna <annagennadinik@gmail.com>
2025-01-08 11:40:14 -0800

    added soft story api

--- api/index.py
+++ api/index.py
@@ -2,22 +2,24 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.api.routers import (
     liquefaction_api,
-    tsunami_api
+    tsunami_api,
+    soft_story_api
 )
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json", redirect_slashes=False)
 
-app.add_middleware(
+'''app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Adjust to your needs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+)'''
 
 app.include_router(liquefaction_api.router)
 app.include_router(tsunami_api.router)
+app.include_router(soft_story_api)
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
