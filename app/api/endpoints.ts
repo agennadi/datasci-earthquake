1f638341db126ea9b07cd0bc954338b376c6eb79
Anna <annagennadinik@gmail.com>
2025-01-28 16:22:59 -0800

    new if clause

--- app/api/endpoints.ts
+++ app/api/endpoints.ts
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || `https://${process.env.VERCEL_URL}/api/py`;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL ||
+(process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/py` : "http://localhost:8000/api/py");
 
 export const ENDPOINTS = {
   softStories: `${BASE_URL}/soft-stories`,
