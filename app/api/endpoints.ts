9018892d32798968bd2cbe7239eacfccddc5b9e0
Anna <annagennadinik@gmail.com>
2025-01-29 17:36:22 -0800

    only one path

--- app/api/endpoints.ts
+++ app/api/endpoints.ts
@@ -1,21 +1,4 @@
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") {
-    // Running on the client
-    return "";
-  }
-
-  if (process.env.VERCEL_ENV === "development") {
-    return "http://localhost:8000/api";
-  }
-
-  if (process.env.VERCEL_ENV === "preview" || process.env.VERCEL_ENV === "production") {
-    return `https://datasci-earthquake.vercel.app/api`;
-  }
-
-  return "http://localhost:8000/api"; // Fallback for local dev
-};
-
-const BASE_URL = getBaseUrl();
+const BASE_URL = 'https://datasci-earthquake.vercel.app/api';
 
 export const ENDPOINTS = {
   softStories: `${BASE_URL}/soft-stories`,
